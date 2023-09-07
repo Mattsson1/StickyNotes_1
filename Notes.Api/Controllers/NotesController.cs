3f7a53102a557abfee2a73d48518981ac4ae650e
DanielM <danielmattsson2010hotmail.com>
2023-09-07 09:51:00 +0200

    SQL Injection fix, by using prepared sql-statements.

--- Notes.Api/Controllers/NotesController.cs
+++ Notes.Api/Controllers/NotesController.cs
@@ -34,7 +34,6 @@ public class NotesController : ControllerBase
         var user = BasicAuthenticationHandler.GetUserFrom(authorizationHeader);
 
         return _database.Notes
-            return _database.Notes
             .Where(note => note.Author == user.Username)
             .Where(note => string.IsNullOrEmpty(containing) ? true : note.Content.Contains(containing))
             .OrderBy(note => note.Id)
